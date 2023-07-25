c0934508b1d70b1be5e5298d85518d1d552be051
Artiiii <31152341+Artiiii@users.noreply.github.com>
2023-07-25 22:31:33 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "AKSres"
-    storage_account_name = "storge23"
+    storage_account_name = "stroge23"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
   }
