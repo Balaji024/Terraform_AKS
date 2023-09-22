ea9620f84afdff2516e32aa868cc8b2eec0f5f74
Balaji024 <36394001+Balaji024@users.noreply.github.com>
2023-09-22 21:54:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 provider "azurerm" { 
 skip_provider_registration = false
+required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
 features {
   }
 }
