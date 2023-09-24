0ca9bdefd38422681029d14ec31f35b0a17bcdf6
Artiiii <Arti04294@gmail.com>
2023-09-24 13:33:17 +0530

    added scipt for creating storage account and resource group

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "aks-poc"
-    storage_account_name = "akspoclearning"
+    resource_group_name  = "terraform"
+    storage_account_name = "infybackendpoc"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
   }
